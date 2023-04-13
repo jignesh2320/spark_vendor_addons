096d79632aabc938cc5b9277ef263d17ad05a7b0
spkal01 <kalligeross@gmail.com>
2023-04-13 20:39:54 +0300

    addons: Checkout Fonts and icons from Ricedroid

--- config.mk
+++ config.mk
@@ -20,3 +20,139 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/addons/overlay/common
 # Navbar
 PRODUCT_PACKAGES += \
     GesturalNavigationOverlayHidden
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontBariolOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvolveSansOverlay \
+    FontFluidSansOverlay \
+    FontGoogleSansOverlay \
+    FontHarmonySansOverlay \
+    FontJTLeonorOverlay \
+    FontLetteraOverlay \
+    FontLinotteOverlay \
+    FontMiSansOverlay \
+    FontNokiaPureOverlay \
+    FontNothiingDotOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontOppoSansOverlay \
+    FontProductSansVHOverlay \
+    FontRobotoCondensedOverlay \
+    FontRosemaryOverlay \
+    FontRubikOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSamsungOneOverlay \
+    FontSimpleDaySourceOverlay \
+    FontSonySketchOverlay
+
+# Icon Packs
+PRODUCT_PACKAGES += \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackPUIAndroidOverlay \
+    IconPackPUISystemUIOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackOOSAndroidOverlay \
+    IconPackOOSSystemUIOverlay \
+    IconPackOutlineAndroidOverlay \
+    IconPackOutlineSystemUIOverlay \
+    IconPackAcherusAndroidOverlay \
+    IconPackAcherusSystemUIOverlay
+
+# Icon Shapes
+PRODUCT_PACKAGES += \
+    IconShapeCloudyOverlay \
+    IconShapeCloudyRoundedOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeCylinderRoundedOverlay \
+    IconShapeFlowerOverlay \
+    IconShapeFlowerRoundedOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHeartRoundedOverlay \
+    IconShapeHexagonOverlay \
+    IconShapeLeafRoundedOverlay \
+    IconShapePebbleRoundedOverlay \
+    IconShapeLeafOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedHexagonOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeStretchedOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay \
+    RohieIconMeowOverlay \
+    DefaultButSquareOverlay \
+    RiceBallsOverlay
+
+# Navbar styles
+PRODUCT_PACKAGES += \
+    NavbarAndroidOverlay \
+	NavbarAsusOverlay \
+	NavbarDoraOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarSammyOverlay \
+    NavbarTecnoCamonOverlay
+
+# Signal / WiFi Icons
+PRODUCT_PACKAGES += \
+	AcherusWiFiOverlay \
+	AcherusSignalOverlay \
+	AquariumSignalOverlay \
+	BarsSignalOverlay \
+	BarsWiFiOverlay \
+	DeepSignalOverlay \
+	DoraSignalOverlay \
+	DoraWiFiOverlay \
+	HuaweiSignalOverlay \
+	IosSignalOverlay \
+	InsideSignalOverlay \
+	InsideWiFiOverlay \
+	OutlineWiFiOverlay \
+	OutlineSignalOverlay \
+	PillsSignalOverlay \
+	PavlovaWiFiOverlay \
+	PUISignalOverlay \
+	RoundSignalOverlay \
+	RoundWiFiOverlay \
+	SneakySignalOverlay \
+	SneakyWiFiOverlay \
+	StrokeSignalOverlay \
+	StrokeWiFiOverlay \
+	WavySignalOverlay \
+	WavyWiFiOverlay \
+	WeedWiFiOverlay \
+	XperiaSignalOverlay \
+	XperiaWiFiOverlay \
+	ZigZagSignalOverlay \
+	ZigZagWiFiOverlay
+
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
